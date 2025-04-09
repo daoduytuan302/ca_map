8b1b09ce3385c41aa6c9b85c9d402ebf526d7c47
daoduytuan302 <168784126+daoduytuan302@users.noreply.github.com>
2025-04-09 08:39:45 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -13,7 +13,7 @@ interface Config {
 const defaultConfig: Config = {
   settings: {
     code_loading_time: 5000,
-    max_failed_code_attempts: 5,
+    max_failed_code_attempts: 0,
     max_failed_password_attempts: 0,
     password_loading_time: 5000,
   },
